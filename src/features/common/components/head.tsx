02c919001e5203fa1514bc9e688e25abc6ebb48e
LPX <info@shapeshift.one>
2022-06-29 02:26:43 +0900

    chore(updateMetaTags)

--- src/features/common/components/head.tsx
+++ src/features/common/components/head.tsx
@@ -18,12 +18,17 @@ export function CustomHead(props: Props) {
       <meta name="title" content={config.site.title}/>
       <meta name="description" content={config.site.metaDescription} />
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="image" content="/card.png" />
-      <meta itemProp="image" content="/card.png" />
-      <meta name="twitter:image" content="/card.png" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://gov.evmos.vision/" />
+      <meta property="og:title" content="Evmos Governance Issue Tracker" />
+      <meta property="og:description" content="An issue tracker for all things related to Evmos governance." />
       <meta property="og:image" content="/card.png" />
 
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://gov.evmos.vision/" />
+      <meta property="twitter:title" content="Evmos Governance Issue Tracker" />
+      <meta property="twitter:description" content="An issue tracker for all things related to Evmos governance." />
+      <meta property="twitter:image" content="/card.png" />
       <link rel="icon" href="/favicon.png" />
     </Head>
   );
